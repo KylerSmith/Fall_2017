558ff812abc605078003a42f2f1a7568f41f04df
kylersmith <kylerasmith@gmail.com>
2017-09-21 17:59:43 -0700

    Update assign5

--- SER321/Assigns/Assign5/scripts/get.sh
+++
@@ -1,1 +0,0 @@
-curl --data "{\"jsonrpc\": \"2.0\", \"method\": \"get\", \"params\":  [\"Squatters Rights\"], \"id\": 3}" localhost:8080
--- SER321/Assigns/Assign5/scripts/remove.sh
+++
@@ -1,1 +0,0 @@
-curl --data "{\"jsonrpc\": \"2.0\", \"method\": \"remove\", \"params\":  [\"HomeBrew\"], \"id\": 3}" localhost:8080
