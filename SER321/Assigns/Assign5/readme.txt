3cc13e04c58532f80657419f388c2b279952187a
kylersmith <kylerasmith@gmail.com>
2017-09-23 22:06:36 -0700

    Assign5

---
+++ SER321/Assigns/Assign5/readme.txt
@@ -0,0 +1,12 @@
+/**
+ * This server can be built with the command
+ * 	ant build.java.server
+ *
+ **/
+
+
+
+
+
+
+
--- SER321/Assigns/Assign5/src/cpp/test.cpp
+++ SER321/Assigns/Assign5/src/cpp/test.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
-#include "stubclient.h"
+#include "movierpcmethods.h"
 #include <jsonrpccpp/client/connectors/httpclient.h>
+
+using namespace std;
+using namespace jsonrpc;
 
 int main() {
 
 	HttpClient httpclient("http://localhost:8080");
-	StubClient c(httpclient);
+	MovieRPCMethods c(httpclient);
 	try {
-		cout << c.sayHello("Peter") << endl;
-		c.notifyServer();
+		cout << c.getTitles() << endl;
 	} catch(JsonRpcException e) {
 		cout << e.what() << endl;
 	}
